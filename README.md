c5c32da3197629bc96aa6c048e829d5651869262
veera venky <veeravenky222@gmail.com>
2018-07-19 21:47:13 +0530

    Added challenge for linter

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+
+```bash
+ $node ./get_repositories_for.js veera83372
+
+'veera83372' has 3 repositories
+1. mijook
+2. debuts
+3. dotfiles
+
+```
---
+++ get_repositories_for.js
@@ -0,0 +1,9 @@
+function printRepos(repos) {
+console.log(repos);
+  console.log("please dont expect me to print repos");
+console.log("Showing talent ")
+console.log("we don't believe in indentation given by editor  ")
+
+}
+
+printRepos(`" ${process.argv[2]}" interesting you dont't have repos `)
